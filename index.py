a85deeac5df7510a04110d1ca132c348669a5c0b
Shreesh Shukla <shreesh8210@gmail.com>
2023-03-22 23:02:15 -0800

    Update index.py

--- index.py
+++ index.py
@@ -3,21 +3,66 @@ import requests
 import datetime
 import time
 import smtplib
+from selenium import webdriver
+from selenium.webdriver.common import keys
+import pandas as pd
 
-#Connecting to web
-for i in range(1,10):
-    i+=1
-URL=f"https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_{i}"
+# Connecting to web
 
-headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
+URL = f"https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_1"
 
-page = requests.get(URL, headers=headers)
+Headers = ({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+           'Accept-Language': 'en-US, en;q=0..5'})
 
-soup1 = BeautifulSoup(page.content, "html.parser")
-print(soup1)
+page = requests.get(URL, headers=Headers)
 
-soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+x = page.content
+# print(x)
 
-title = soup2.find(id='anonCarousel1').get_text()
+soup = BeautifulSoup(page.content, "html.parser")
+# print(soup1)
 
-print(title)
+
+# soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+node = []
+mode = []
+dictionary = {}
+nedo=[]
+for i in range(0,19):
+    dataframe = [f"Item{i}"]
+    nedo.append(dataframe)
+    
+print(nedo)
+
+red=input("Enter The Excel file name in which data will be saved:")
+
+for i in range(0, 19):
+    
+    links = soup.find_all("a", attrs={
+                          'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
+    URLin = links[0].get('href')
+    URLout = "https://amazon.com" + URLin
+    
+    # print(node)
+    
+    node.append(URLout)
+    # print(URLout)
+    # print(node)
+    mode.append(node)
+    i=i + 1
+print(mode)
+
+for i in range(0,19):
+    k=nedo[i]
+    o=mode[i]
+    dictionary[k]=o
+    i=i+1
+print(dictionary)
+    
+data=pd.DataFrame(dictionary)
+print(data)
+
+s=data.to_csv(f"C:\\Users\\om\\Desktop\\{red}.csv")
+print(s)
+
+# print(title)
